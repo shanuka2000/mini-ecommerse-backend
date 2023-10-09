dcbcc709502fd035c164bd08de4da29b212791b4
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-10 00:59:52 +0530

    Added mongo config and product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
+    ProductsModule,
     MongooseModule.forRoot(
-      'mongodb+srv://shanukase:8Ejay0sufP6hTbOM@cluster0.yj3jrtg.mongodb.net/eCommercedb?retryWrites=true&w=majority',
+      'mongodb+srv://shanukase:8Ejay0sufP6hTbOM@cluster0.yj3jrtg.mongodb.net/mini-ecommerce-db?retryWrites=true&w=majority',
     ),
   ],
   controllers: [AppController],
