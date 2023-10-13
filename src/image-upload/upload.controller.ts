670037205ea25b64c53a9eb74cb7972ae2f14238
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-13 22:22:28 +0530

    Implemented file upload endpoint

---
+++ src/image-upload/upload.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { UploadService } from './upload.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseInterceptors(
+    FilesInterceptor('file', null, {
+      storage: diskStorage({
+        destination: 'public/img',
+        filename: (req, file, cb) => {
+          const fileNameSplit = file.originalname.split('.');
+          const fileExt = fileNameSplit[fileNameSplit.length - 1];
+          cb(null, `${Date.now()}.${fileExt}`);
+        },
+      }),
+    }),
+  )
+  async uploadFiles(@UploadedFiles() file: Array<Express.Multer.File>) {
+    return file;
+  }
+}
