47ab811da34cb86bff5dc7ba30cf23489d45b7b7
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-13 22:24:07 +0530

    Imported ServeStaticModule and uploadModule in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
+import { UploadModule } from './image-upload/upload.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ProductsModule,
+    UploadModule,
     MongooseModule.forRoot(
       'mongodb+srv://shanukase:8Ejay0sufP6hTbOM@cluster0.yj3jrtg.mongodb.net/mini-ecommerce-db?retryWrites=true&w=majority',
     ),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
